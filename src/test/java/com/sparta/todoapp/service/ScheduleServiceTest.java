f0da513dc133397b2b80858c17510dd33a6dbaa4
tktcod <hs060100@gmail.com>
2024-02-21 15:00:06 +0900

    # test : ScheduleServiceTest.updateSchedule updateTaskCompletionSchedule method success test 추가

--- src/test/java/com/sparta/todoapp/service/ScheduleServiceTest.java
+++ src/test/java/com/sparta/todoapp/service/ScheduleServiceTest.java
@@ -137,6 +137,59 @@ class ScheduleServiceTest {
             // Then
             assertEquals("해당 할 일 작성자가 아닙니다.", exception.getMessage());
         }
+
+        @Test
+        @DisplayName("updateTaskCompletionSchedule success 1 - 미완료 할일 완료로 수정")
+        public void updateTaskCompletionScheduleSuccess1() {
+            // Given
+            Long scheduleId = 1L;
+
+            Boolean finished = true;
+            Boolean notFinished = false;
+
+            User user = new User();
+            user.setId(1L);
+
+            Schedule schedule = new Schedule();
+            schedule.setId(scheduleId);
+            schedule.setUser(user);
+            schedule.setIsFinished(notFinished);
+
+
+            given(scheduleRepository.findById(scheduleId)).willReturn(Optional.of(schedule));
+
+            // When
+            ScheduleResponseDto responseDto = scheduleService.updateTaskCompletionSchedule(scheduleId, finished, user);
+
+            // Then
+            assertEquals(finished, responseDto.getIsFinished());
+        }
+
+        @Test
+        @DisplayName("updateTaskCompletionSchedule success 2 - 완료된 할일 미완료로 수정")
+        public void updateTaskCompletionScheduleSuccess2() {
+            // Given
+            Long scheduleId = 1L;
+
+            Boolean finished = true;
+            Boolean notFinished = false;
+
+            User user = new User();
+            user.setId(1L);
+
+            Schedule schedule = new Schedule();
+            schedule.setId(scheduleId);
+            schedule.setUser(user);
+            schedule.setIsFinished(finished);
+
+            given(scheduleRepository.findById(scheduleId)).willReturn(Optional.of(schedule));
+
+            // When
+            ScheduleResponseDto responseDto = scheduleService.updateTaskCompletionSchedule(scheduleId, notFinished, user);
+
+            // Then
+            assertEquals(notFinished, responseDto.getIsFinished());
+        }
     }
 
     @Nested
@@ -180,27 +233,27 @@ class ScheduleServiceTest {
             // Then
             assertEquals("해당 할 일이 존재하지 않습니다.", exception.getMessage());
         }
-    }
 
-    @Test
-    @DisplayName("getAllSchedule method")
-    public void getAllSchedule() {
-        // Given
-        List<Schedule> mockScheduleList = new ArrayList<>();
-        User mockUser = new User();
-        mockScheduleList.add(new Schedule(new ScheduleRequestDto("Title 1", "Content 1"), mockUser));
-        mockScheduleList.add(new Schedule(new ScheduleRequestDto("Title 2", "Content 2"), mockUser));
+        @Test
+        @DisplayName("getAllSchedule method")
+        public void getAllSchedule() {
+            // Given
+            List<Schedule> mockScheduleList = new ArrayList<>();
+            User mockUser = new User();
+            mockScheduleList.add(new Schedule(new ScheduleRequestDto("Title 1", "Content 1"), mockUser));
+            mockScheduleList.add(new Schedule(new ScheduleRequestDto("Title 2", "Content 2"), mockUser));
 
-        given(scheduleRepository.findAllByOrderByCreatedAtDesc()).willReturn(mockScheduleList);
+            given(scheduleRepository.findAllByOrderByCreatedAtDesc()).willReturn(mockScheduleList);
 
-        // When
-        List<ScheduleResponseDto> responseDtoList = scheduleService.getAllSchedule();
+            // When
+            List<ScheduleResponseDto> responseDtoList = scheduleService.getAllSchedule();
 
-        // Then
-        assertEquals(2, responseDtoList.size());
-        assertEquals("Title 1", responseDtoList.get(0).getTitle());
-        assertEquals("Content 1", responseDtoList.get(0).getContents());
-        assertEquals("Title 2", responseDtoList.get(1).getTitle());
-        assertEquals("Content 2", responseDtoList.get(1).getContents());
+            // Then
+            assertEquals(2, responseDtoList.size());
+            assertEquals("Title 1", responseDtoList.get(0).getTitle());
+            assertEquals("Content 1", responseDtoList.get(0).getContents());
+            assertEquals("Title 2", responseDtoList.get(1).getTitle());
+            assertEquals("Content 2", responseDtoList.get(1).getContents());
+        }
     }
 }
