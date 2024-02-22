2a35b540f591add021e5681ab2b014f6062afe91
tktcod <hs060100@gmail.com>
2024-02-22 12:16:12 +0900

    fix(dto) : requestDto @NotNull 조건 추가

--- src/main/java/com/sparta/todoapp/dto/LoginRequestDto.java
+++ src/main/java/com/sparta/todoapp/dto/LoginRequestDto.java
@@ -1,11 +1,14 @@
 package com.sparta.todoapp.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class LoginRequestDto {
+    @NotNull
     private String username;
+    @NotNull
     private String password;
 }
--- src/main/java/com/sparta/todoapp/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/todoapp/dto/SignupRequestDto.java
@@ -1,15 +1,19 @@
 package com.sparta.todoapp.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+    @NotNull
     @Pattern(regexp="^[a-z0-9]{4,10}$")
     private String username;
 
+    @NotNull
     @Pattern(regexp="^[a-zA-Z0-9]{8,15}$")
     private String password;
 }
